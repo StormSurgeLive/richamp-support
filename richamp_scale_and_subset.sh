b9b80aef1993d14e042eec93de2998931289b634
Jason Fleming <jason.fleming@seahorsecoastal.com>
2022-07-14 15:08:32 -0400

    Fixups for logging

--- richamp_scale_and_subset.sh
+++ richamp_scale_and_subset.sh
@@ -12,8 +12,11 @@ SCENARIODIR=$ADVISDIR/$ENSTORM # Should also be PWD, but I would rather not assu
 logfile=${0}.log
 targetScript="richamp_scale_and_subset.scr"
 jobCheckIntervalSeconds=60
+for ending in submit start finish error ; do
+    rm -f $targetScript.$ending # remove .submit .start .finish and .error if they are left over
+done
 #
-echo "[$(date +'%Y-%h-%d-T%H:%M:%S%z')] $0: Submitting $targetScript $asgs_python $SCENARIODIR" >> $logfile
+echo "[$(date +'%Y-%h-%d-T%H:%M:%S%z')] $0: Submitting $targetScript $asgs_python $SCENARIODIR" > $targetScript.submit | tee --append $logfile
 sbatch $targetScript $asgs_python $SCENARIODIR 2>>jobErr >jobID
 # check to see if the sbatch command succeeded; you can also add a retry
 # but maybe not necessary
@@ -21,14 +24,15 @@ if [[ $? == 0 ]]; then
     echo "[$(date +'%Y-%h-%d-T%H:%M:%S%z')] $0: Job ID for $targetScript is $(<jobID)" >> $logfile
 else
     echo "[$(date +'%Y-%h-%d-T%H:%M:%S%z')] $0: The sbatch command failed for $targetScript." >> $logfile
+    exit
 fi
 # wait for batch job to start
 until [[ -e $targetScript.start ]]; do
-    echo "[$(date +'%Y-%h-%d-T%H:%M:%S%z')] Waiting for $targetScript to start."
+    echo "[$(date +'%Y-%h-%d-T%H:%M:%S%z')] Waiting for $targetScript to start." >> $logfile
     sleep $jobCheckIntervalSeconds
 done
 # wait for batch job to finish successfully or exit with an error
 until [[ -e $targetScript.finish || -e $targetScript.error ]]; do
-    echo "[$(date +'%Y-%h-%d-T%H:%M:%S%z')] Waiting for $targetScript to finish."
+    echo "[$(date +'%Y-%h-%d-T%H:%M:%S%z')] Waiting for $targetScript to finish." >> $logfile
     sleep $jobCheckIntervalSeconds
 done
